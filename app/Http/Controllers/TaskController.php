a69b3a4e0ddd9b105e0c41dd0b2fb67185b27be9
Orizin <nlgporizin10@gmail.com>
2022-02-02 12:24:58 +0900

    一覧表示の実装

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
@@ -13,7 +14,8 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+        $tasks = Auth::user()->task;
+        return view('tasks.index', compact('tasks'));
     }
 
     /**
