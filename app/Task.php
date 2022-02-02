5465cca33c87910ce7e7c503fc9546b449f89665
Orizin <nlgporizin10@gmail.com>
2022-02-02 12:03:39 +0900

    モデルのリレーション

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    /**
+     * リレーション
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
+
--- app/User.php
+++ app/User.php
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * リレーション
+     */
+    public function task()
+    {
+        return $this->hasMany("App\Task");
+    }
 }
